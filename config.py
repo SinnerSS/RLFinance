4ed21f900222fe2cc94e8d590ae28283babdfbf3
Sinner <ntnbusinf@gmail.com>
2025-04-22 19:09:50 +0700

    remove unnecessary fields

--- config.py
+++ config.py
@@ -8,17 +8,16 @@ class Config:
     pool_size: int = 300
     pool_strategy: Literal['mixed', 'top', 'bot'] = 'mixed'
 
-    data_path: Path = field(default_factory=lambda: Path('data').resolve()) 
     result_path: Path = field(default_factory=lambda: Path('result').resolve())
 
-    start_train: pd.Timestamp = field(default_factory=lambda: pd.to_datetime('2016-01-01'))
-    end_train: pd.Timestamp = field(default_factory=lambda: pd.to_datetime('2020-12-31'))
-    start_val: pd.Timestamp = field(default_factory=lambda: pd.to_datetime('2021-01-01'))
-    end_val: pd.Timestamp = field(default_factory=lambda: pd.to_datetime('2021-12-31'))
-    start_test: pd.Timestamp = field(default_factory=lambda: pd.to_datetime('2022-01-01'))
-    end_test: pd.Timestamp = field(default_factory=lambda: pd.to_datetime('2023-12-28'))
+    start_train: str = '2016-01-01'
+    end_train: str = '2020-12-31'
+    start_val: str = '2021-01-01'
+    end_val: str = '2021-12-31'
+    start_test: str = '2022-01-01'
+    end_test: str = '2023-12-28'
 
 
     def __post_init__(self):
-        assert self.data_path.is_dir(), 'Data directory do not exists!'
         self.result_path.mkdir(parents=True, exist_ok=True)
+        self.cwd = Path.cwd()
